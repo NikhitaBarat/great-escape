fcac55cb6a3d1592f360363cf9172e96c1bc7d9b
Nikhita <bnikhita2003@gmail.com>
2022-06-11 19:53:38 +0530

    added auth0

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,25 @@ import {
   ApolloProvider
 } from "@apollo/client";
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
+import history from "./utils/history";
+import { getConfig } from "./config";
+
+const onRedirectCallback = (appState) => {
+  history.push(
+    appState && appState.returnTo ? appState.returnTo : window.location.pathname
+  );
+};
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  ...(config.audience ? { audience: config.audience } : null),
+  redirectUri: window.location.origin,
+  onRedirectCallback,
+};
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -21,7 +40,9 @@ root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <App />
+        <Auth0Provider {...providerConfig}>
+          <App />
+        </Auth0Provider>
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
