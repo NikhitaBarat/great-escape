cdd94e57525853ed3be726ec54979ffa73f1df5c
Nikhita <bnikhita2003@gmail.com>
2022-06-11 11:21:37 +0530

    modified header

--- client/src/components/header/header.components.jsx
+++ client/src/components/header/header.components.jsx
@@ -3,13 +3,14 @@ import './header.styles.css'
 const Header = () => {
   return (
     <div className='navbar'>
+      <div></div>
       <div className='nav_options'>
         <ul>
           <li><a href='/'>ABOUT US</a></li>
-          <li><a href='/about'>GALLERY</a></li>
-          <li><a href='/projects'>ACTIVITIES</a></li>
-          <li><a href='/resume'>HOTELS</a></li>
-          <li><a href='/contact'>TRIPS</a></li>
+          <li><a href='/gallery'>GALLERY</a></li>
+          <li><a href='/activities'>ACTIVITIES</a></li>
+          <li><a href='/hotels'>HOTELS</a></li>
+          <li><a href='/trip'>TRIPS</a></li>
         </ul>
         </div>
         <div className='signin_options'>
