14302321d3f21ddd93854d19f3bf17cd9f41a800
siddhantprateek <siddhantprateek@gmail.com>
2022-06-11 15:32:32 +0530

    home page route

--- client/src/pages/home/home.pages.jsx
+++ client/src/pages/home/home.pages.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './home.styles.css';
+import { Homebg, Plane, Sky } from '../../assets';
 
 const Home = () => {
   return (
     <div className='home-pages'>
-      Home
+    <div className="top-section">
+      <div className="home-container">
+        <img className='bg-image' src={Homebg} alt="" />
+      </div>
+      <div className="home-page-content">
+        <h1>Great Escape</h1>
+        <p>find personalized recommendations for activities, hotels and more</p>
+      </div>
+    </div>
+    <div className="middle-section">
+      <div className="home-container">
+        <img className='bg-sky' src={Sky} alt="" />
+      </div>
+      <img src={Plane} className="plane" alt="" />
+      <h1>Hotels and <br/> Accomodation</h1>
+    </div>
     </div>
   )
 }
