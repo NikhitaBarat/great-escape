2ab647a88da57aa57f9c8b81964a09f09d9dc6c4
siddhantprateek <siddhantprateek@gmail.com>
2022-06-11 15:39:41 +0530

    activity default export

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,6 @@
 export { default as Hotels } from './hotels/hotels.pages';
 export { default as Trip  } from './trip/trip.pages';
 export { default as Home } from './home/home.pages';
+export { default as Activities } from './activities/activities.pages';
+
  
