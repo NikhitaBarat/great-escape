7b756a8bfd61eed57d9d006093aad749f3779f6d
siddhantprateek <siddhantprateek@gmail.com>
2022-06-11 10:49:43 +0530

    graphql client fixed

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,13 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider
-} from '@apollo/client'
+} from "@apollo/client";
 
-const client = ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache()
-})
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
