29eba4e0a05d5e05b8407651491a82a94ad1b94b
Nikhita <bnikhita2003@gmail.com>
2022-06-11 13:33:42 +0530

    created hotelcard elements

--- client/src/components/hotelCard/hotelcard.components.jsx
+++ client/src/components/hotelCard/hotelcard.components.jsx
@@ -1,9 +1,183 @@
 import React from 'react'
 import './hotelcard.styles.css'
+import { cards8, cards9, cards10, cards11, cards12, cards13, cards14, cards15, cards16, cards17 } from '../../assets'
 
 const Hotelcard = () => {
   return (
-    <div>Hotelcard</div>
+    <div>
+      <ul class="cards">
+        <li>
+          <div href="" class="card">
+            <img src= {cards8} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">MV, Maldives</h3>
+                  <span class="card__status">$400 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>1,677 km away <br /> 14–19 Jun</span>
+                </div>
+              </div>
+              <p class="card__description">Adaaran Club Rannalhi is featured among the best hotels in Maldives and sits exclusively at the tip of the South Male atoll within the exotic collection of islands known as the Maldives.</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards9} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">Badachro, UK</h3>
+                  <span class="card__status">$657 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>7,557 km away <br /> 1-6 Nov</span>
+                </div>
+              </div>
+              <p class="card__description">Quant self-catering cabin gently nestled into the historic family-run Dry Island. "Otter Cabin" is fitted out to a high standard with a double bedroom, shower room, open plan kitchen and living room and its own hot tub and decking area.</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards10} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">Provincia de Cartagena, Bolívar, Colombia</h3>
+                  <span class="card__status">$566 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>2,477 km away <br />1-6 Feb</span>
+                </div>
+              </div>
+              <p class="card__description">The Rosario Islands are an archipelago located in the Caribbean Sea consisting of 28 islands. Enjoy our exclusive private island located in the Rosario Islands. The place has a tropical style that combines with the beautiful landscape, perfect to disconnect from everyday life while surrounded by a sea of ​​seven colors.</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards11} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">Gildeskål, Nordland, Norway</h3>
+                  <span class="card__status">$1000 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>5,670 km away <br /> 25 - 30 Jun</span>
+                </div>
+              </div>
+              <p class="card__description">Head north in Norway and experience our tiny-hotel on a remote island. The Arctic Hideaway is an architecturally playful and stunning collection of ten individual buildings, where four are sleeping cabins. </p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards12} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">MV, Maldives</h3>
+                  <span class="card__status">$400 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>1,677 km away <br /> 14–19 Jun</span>
+                </div>
+              </div>
+              <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards13} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">MV, Maldives</h3>
+                  <span class="card__status">$400 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>1,677 km away <br /> 14–19 Jun</span>
+                </div>
+              </div>
+              <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards14} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">MV, Maldives</h3>
+                  <span class="card__status">$400 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>1,677 km away <br /> 14–19 Jun</span>
+                </div>
+              </div>
+              <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards15} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">MV, Maldives</h3>
+                  <span class="card__status">$400 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>1,677 km away <br /> 14–19 Jun</span>
+                </div>
+              </div>
+              <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards16} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">MV, Maldives</h3>
+                  <span class="card__status">$400 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>1,677 km away <br /> 14–19 Jun</span>
+                </div>
+              </div>
+              <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="" class="card">
+            <img src={cards17} class="card__image" alt="" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <div class="card__header-text">
+                  <h3 class="card__title">MV, Maldives</h3>
+                  <span class="card__status">$400 per night</span>
+                </div>
+                <div className='card__right'>
+                  <span>1,677 km away <br /> 14–19 Jun</span>
+                </div>
+              </div>
+              <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
