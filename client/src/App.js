7749cbc3742aec30b62587d57d12dcdee2212c33
suvangipaul <suvangipaul8@gmail.com>
2022-06-11 16:11:58 +0530

    Gallery Routed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/header/header.components.jsx";
 import { Routes, Route } from "react-router-dom";
-import { Trip, Hotels, Home, Activities, Profile } from "./pages";
+import { Trip, Hotels, Home, Activities, Profile, Gallery } from "./pages";
 import "./App.css";
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
         <Route path="/activities" element={<Activities />} />
         <Route path="/hotels" element={<Hotels />} />
         <Route path="/trip" element={<Trip />} />
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Trip } from "./trip/trip.pages";
 export { default as Home } from "./home/home.pages";
 export { default as Activities } from "./activities/activities.pages";
 export { default as Profile } from "./profile/profile.pages";
+export { default as Gallery } from "./gallery/gallery.pages";
