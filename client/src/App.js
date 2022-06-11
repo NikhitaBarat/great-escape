ab5abf9fd6afede211db9377c3186479b68c4c66
siddhantprateek <siddhantprateek@gmail.com>
2022-06-11 15:30:51 +0530

    home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Header from './components/header/header.components.jsx'
 import { Routes, Route } from 'react-router-dom'
-import { Hotels } from './pages';
-import { Trip } from './pages';
+import { Trip, Hotels, Home } from './pages';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-      <Route path="/hotels" element={<Hotels />}/>
-      <Route path="/trip" element={<Trip />}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/hotels" element={<Hotels />}/>
+        <Route path="/trip" element={<Trip />}/>
       </Routes>
     </div>
   );
