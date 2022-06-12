33fcfe637d201eab61c28facfbee4206a771a8d1
siddhantprateek <siddhantprateek@gmail.com>
2022-06-12 16:09:47 +0530

    activities pages

--- client/src/pages/activities/activities.pages.jsx
+++ client/src/pages/activities/activities.pages.jsx
@@ -7,17 +7,27 @@ import Cruising from "../../assets/cruising.png";
 function Activities() {
   return (
     <div className="activities">
-      <div className="water">
-        <div className="water1">
-          <img className="waterimage" src={JetSki} alt="" />
-          <h2>JET SKI</h2>
-        </div>
-        <div className="water2">
-          <img className="waterimage" src={Cruising} alt="" />
-          <h2>CRUISING</h2>
+      <div className="book-bg-image-act">
+        <div className="inner-bg">
+          <div>
+            <h1>BOOK A TRIP WITH US</h1>
+            <button className="book-trip-btn">BOOK NOW</button>
+          </div>
         </div>
       </div>
-      <ActivityCard />
+      <div className="activity-container">
+        <div className="water">
+          <div className="water1">
+            <img className="waterimage" src={JetSki} alt="" />
+            <h2>JET SKI</h2>
+          </div>
+          <div className="water2">
+            <img className="waterimage" src={Cruising} alt="" />
+            <h2>CRUISING</h2>
+          </div>
+        </div>
+        <ActivityCard />
+      </div>
     </div>
   );
 }
