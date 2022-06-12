f989cfc0239855238f637af26856c02e3fd0c5fe
Nikhita <bnikhita2003@gmail.com>
2022-06-12 14:13:25 +0530

    added footer

--- client/src/components/footer/footer.components.jsx
+++ client/src/components/footer/footer.components.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./footer.styles.css";
-// import FacebookIcon from '@mui/icons-material/Facebook';
-// import TwitterIcon from '@mui/icons-material/Twitter';
-// import InstagramIcon from '@mui/icons-material/Instagram';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 
 const Footer = () => {
   return (
@@ -13,50 +14,49 @@ const Footer = () => {
           <div class="container">
             <div class="row">
               <div class=" item">
-                <h2>Services</h2>
+                <h2>Quick Links</h2>
                 <ul>
-                  <li>
-                    <a href="/">Web design</a>
-                  </li>
-                  <li>
-                    <a href="/">Development</a>
-                  </li>
-                  <li>
-                    <a href="/">Hosting</a>
-                  </li>
+                  <li><a href="/">About Us</a></li>
+                  <li><a href="/">Your Profile</a></li>
+                  <li><a href="/">Pricing</a></li>
+                  <li><a href="/">Contact</a></li>
                 </ul>
               </div>
               <div class=" item">
-                <h2>About</h2>
+                <h2>Useful Links</h2>
                 <ul>
-                  <li>
-                    <a href="/">Company</a>
-                  </li>
-                  <li>
-                    <a href="/">Team</a>
-                  </li>
-                  <li>
-                    <a href="/">Careers</a>
-                  </li>
+                  <li><a href="/">Privacy Policy</a></li>
+                  <li><a href="/">Terms and Conditions</a></li>
+                  <li><a href="/">Disclaimer</a></li>
+                  <li><a href="/">Pricing</a></li>
+                </ul>
+              </div>
+              <div class=" item">
+                <h2>Legal Terms</h2>
+                <ul>
+                  <li><a href="/">Payments</a></li>
+                  <li><a href="/">Subscriptions</a></li>
+                  <li><a href="/">Gift Cards</a></li>
+                  <li><a href="/">Support</a></li>
                 </ul>
               </div>
               <div class=" item text">
                 <h1>Great Escape</h1>
                 <p>
-                  Praesent sed lobortis mi. Suspendisse vel placerat ligula.
-                  Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam
-                  quis tristique lectus. Aliquam in arcu eget velit pulvinar
-                  dictum vel in justo.
+                  Great Escape is a travel planner website. It recommends some features which allows to plan every aspect of your trip, from transport and accommodation to booking hotels, spending time in water sports. It shows numerous booking options, price details, and provides you the option to create your own custom-made travel bucket list.
                 </p>
+                <div class=" item social">
+                  <ul>
+                    <li><a href="#"><FacebookIcon className="fab" /></a></li>
+                    <li><a href="#"><PinterestIcon className="fab" /></a></li>
+                    <li><a href="#"><LinkedInIcon className="fab" /></a></li>
+                    <li><a href="#"><InstagramIcon className="fab" /></a></li>
+                    <li><a href="#"><TwitterIcon className="fab"/></a></li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div class=" item social">
-              {/* <a href="#"><FacebookIcon /></a>
-                  <a href="#"><InstagramIcon /></a>
-                  <a href="#"><TwitterIcon /></a>
-                  <a href="#"><LinkedInIcon /></a> */}
-            </div>
-          </div>
+          </div> <hr />
           <p class="copyright">Great Escape © 2022</p>
         </footer>
       </div>
