8eda2b122eb6f0736503bd103446c4a94af11a99
siddhantprateek <siddhantprateek@gmail.com>
2022-06-12 10:12:54 +0530

    hotel search fixed

--- client/src/pages/hotels/hotels.pages.jsx
+++ client/src/pages/hotels/hotels.pages.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
-import HotelCard from '../../components/hotelCard/hotelcard.components.jsx'
-import SearchIcon from '../../assets/ei_search.png'
-import './hotels.styles.css'
+import React, { useState } from "react";
+import HotelCard from "../../components/hotelCard/hotelcard.components.jsx";
+import SearchIcon from "../../assets/ei_search.png";
+import { useQuery, gql } from "@apollo/client";
+import { HotelSearch } from "../../assets/index.js";
+import "./hotels.styles.css";
 
 const Hotels = () => {
+  const [search, setSearch] = useState("");
+
+  const { loading, error, data } = useQuery(gql`
+    {
+      hotels {
+        hotel_name
+        imgurl
+        despscription
+        rating
+        new_price
+      }
+    }
+  `);
+  if (loading)
+    return (
+      <div className="filter-section">
+        <p>Loading...</p>
+      </div>
+    );
+
+  if (error)
+    return (
+      <div className="filter-section">
+        <p>Error :(</p>
+      </div>
+    );
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredHotel = data.hotels.filter((hotel) =>
+    hotel.hotel_name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
-    <div className='hotels'>
-      <div className='search'>
-        <div className="search-field">
-          <input type="string" placeholder="Search" />
-          <img className='searchimage' src={SearchIcon} alt="" />
+    <div className="hotels">
+      <div className="hotel-bg">
+        <div className="hotel-image-for-search">
+          <h1>Looking For Hotel</h1>
         </div>
       </div>
-      <HotelCard />
+      <div className="search-area">
+        <div className="hotel-search-field">
+          <input type="string" onChange={handleSearch} placeholder="Search" />
+          <img className="searchimage" src={SearchIcon} alt="" />
+        </div>
+      </div>
+      <HotelCard hotels={filteredHotel} />
     </div>
-  )
-}
+  );
+};
 
-export default Hotels
+export default Hotels;
